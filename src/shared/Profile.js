3eb88d92621e079c6aec57eebdca773a6a61ab6a
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-03 14:17:59 -0700

    sign up completed with sign out button being populated accordingly

--- src/shared/Profile.js
+++ src/shared/Profile.js
@@ -1,6 +1,22 @@
 import Component from '../Component.js';
+import { auth } from '../services/firebase.js';
 
 class Profile extends Component {
+    render() {
+        const dom = this.renderDOM();
+        
+        if(!this.props.user) {
+            return dom;
+        }
+        
+        const button = dom.querySelector('button');
+
+        button.addEventListener('click', () => {
+            auth.signOut();
+        });
+
+        return dom;
+    }
     renderTemplate() {
         const user = this.props.user;
         
