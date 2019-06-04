e4a63a7afe66bf00d8296a8d0900a4324118e4e3
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-06-04 14:57:44 -0700

    update endpoint depending on query props.

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -1,12 +1,25 @@
-const URL = 'https://api.themoviedb.org/3/movie/now_playing?';
+const URL = 'https://api.themoviedb.org/3';
+
+const nowPlaying = '/movie/now_playing?';
+
+const search = '/search/movie?';
 
 const key = 'api_key=ad4e796c6d9178454e18a3586be79368';
 
 const movieApi = {
-    getMovies() {
-        return fetch(URL + key)
+    getMovies(queryProps) {
+        let url = '';
+        if(queryProps) {
+            url = `${URL}${search}${key}&query=${queryProps.title}`;
+        } 
+        else {
+            url = `${URL}${nowPlaying}${key}`;
+        }
+
+        return fetch(url)
             .then(response => response.json());
     }
+    
 };
 
 
